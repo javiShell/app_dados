2710c9f465dc790dfbb79878ee4b664a9f6bba48
Francisco Javier Pedrajas <javi1996javi@gmail.com>
2021-01-02 16:52:40 +0100

    primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { applySourceSpanToExpressionIfNeeded } from '@angular/compiler/src/output/output_ast';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dados';
+   title = 'dados';
+   img1 = '../assets/img/dice1.png';
+   img2 = '../assets/img/dice2.png';
+   aleatorio!: number;
+   aleatorio2!: number;
+   iguales = false;
+
+  dados(): void {
+      this.aleatorio = Math.round(Math.random()*5)+1;
+      this.aleatorio2 = Math.round(Math.random()*5)+1;
+      console.log(this.aleatorio);
+      console.log(this.aleatorio2);
+      
+      this.img1 = '../assets/img/dice'+this.aleatorio+'.png';
+      this.img2 = '../assets/img/dice'+this.aleatorio2+'.png';
+
+      if(this.aleatorio === this.aleatorio2 ){
+        this.iguales = true;
+      }else{
+        this.iguales = false;
+      }
+      
+      
+  }
+
+
 }
